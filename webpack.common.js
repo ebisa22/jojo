753e702ea789a7e96231fd08430d88cb184c7171
ebisa <225586468+ebisa22@users.noreply.github.com>
2025-12-30 23:05:59 -0800

    Set up gh-pages

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,8 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js", //content hash use hash and the hash changes if something in the module changes.
     assetModuleFilename: "[name][ext]", //keeps the same name for images when compiled from src to dist
-    clean: true, 
+    clean: true,
+ 
   },
   plugins: [
     new HtmlWebpackPlugin({
